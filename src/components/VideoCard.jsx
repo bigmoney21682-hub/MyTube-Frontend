2b6447ee8f756925e31e0a752cc2d175ea891cbb
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-11 10:56:58 -0500

    Create VideoCard.jsx

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function VideoCard({ video }) {
+  return (
+    <Link to={`/watch/${video.id}`}>
+      <div className="video-card">
+        <img src={video.thumbnail} />
+        <h3>{video.title}</h3>
+        <p>{video.author}</p>
+      </div>
+    </Link>
+  );
+}
