44ec1872ffacb929a9f45383318c77b2c5030baf
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-11 11:01:54 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Watch from "./pages/Watch";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/watch/:id" element={<Watch />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
