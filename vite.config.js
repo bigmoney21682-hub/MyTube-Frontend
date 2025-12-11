620ae8117392970931bdc695a2c7470fdc60be3c
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-11 14:37:05 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/MyTube-Frontend/", // important for GitHub Pages
   server: {
     port: 5173,
     proxy: {
-      "/api": "https://mytube-backend-xlz4.onrender.com"
+      "/api": "https://mytube-backend-xlz4.onrender.com" // live backend
     }
   },
   build: {
