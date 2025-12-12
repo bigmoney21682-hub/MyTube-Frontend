eb91cc02471c1ff07bca25b71ca83c30be1ee904
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-11 22:04:14 -0500

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,7 +4,12 @@ export default function VideoCard({ video }) {
   return (
     <Link to={`/watch/${video.id}`}>
       <div className="video-card">
-        <img src={video.thumbnail} />
+        <img
+          src={video.thumbnail.startsWith("http://")
+            ? video.thumbnail.replace("http://", "https://")
+            : video.thumbnail
+          }
+        />
         <h3>{video.title}</h3>
         <p>{video.author}</p>
       </div>
