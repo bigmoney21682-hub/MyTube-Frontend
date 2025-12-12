779a3851753236fa4773058713ebd528b689c847
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-12 11:11:36 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/MyTube-Frontend/",   // correct for GitHub Pages
+  base: "./",          // <-- relative path for GitHub Pages
   plugins: [react()],
   build: {
     outDir: "dist",
-    rollupOptions: {
-      input: "index.html",     // 🔥 FORCE VITE TO INJECT SCRIPT TAG
-    },
   },
 });
