d34c196a9d14588b2a1738c49687532418176d8d
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-13 12:33:58 -0500

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,18 +1,23 @@
-import { Link } from "react-router-dom";
-
 export default function VideoCard({ video }) {
+  const { id, snippet } = video;
+  const videoId = id.videoId;
+
   return (
-    <Link to={`/watch/${video.id}`}>
-      <div className="video-card">
+    <a href={`#/watch/${videoId}`} style={{ textDecoration: "none", color: "inherit" }}>
+      <div style={{
+        margin: "15px",
+        maxWidth: "360px",
+        cursor: "pointer"
+      }}>
         <img
-          src={video.thumbnail.startsWith("http://")
-            ? video.thumbnail.replace("http://", "https://")
-            : video.thumbnail
-          }
+          src={snippet.thumbnails.medium.url}
+          alt={snippet.title}
+          style={{ width: "100%", borderRadius: "12px" }}
         />
-        <h3>{video.title}</h3>
-        <p>{video.author}</p>
+        <h3 style={{ fontSize: "1.1rem", margin: "10px 0" }}>
+          {snippet.title}
+        </h3>
       </div>
-    </Link>
+    </a>
   );
 }
