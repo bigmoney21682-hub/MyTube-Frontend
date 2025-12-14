5f0b84c9b982eb2ce7156c577a8ad9d44e6bc0a6
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-14 17:41:31 -0500

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function VideoCard({ video }) {
   return (
-    <Link to={`/watch/${video.id}`} className="video-card">
-      <img src={video.thumbnail} alt={video.title} />
-      <h4>{video.title}</h4>
-      <p>{video.uploader}</p>
+    <Link to={`/watch/${video.id}`} style={{ textDecoration: "none" }}>
+      <div className="video-card">
+        <img src={video.thumbnail} alt={video.title} />
+        <h4>{video.title}</h4>
+        <p>{video.uploader}</p>
+      </div>
     </Link>
   );
 }
