225ee5cc99056de2b8a66cfb583406e6caee9061
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-13 22:29:11 -0500

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -13,21 +13,9 @@ export default function VideoCard({ video }) {
         maxWidth: "360px",
         cursor: "pointer"
       }}>
-        <img
-          src={thumbnail}
-          alt={title}
-          style={{ width: "100%", borderRadius: "12px" }}
-        />
-        <h3 style={{
-          fontSize: "1.1rem",
-          margin: "10px 0",
-          lineHeight: "1.3"
-        }}>
-          {title}
-        </h3>
-        <p style={{ margin: 0, color: "#aaa" }}>
-          {uploader}
-        </p>
+        <img src={thumbnail} alt={title} style={{ width: "100%", borderRadius: "12px" }} />
+        <h3 style={{ fontSize: "1.1rem", margin: "10px 0" }}>{title}</h3>
+        <p style={{ margin: 0, color: "#aaa" }}>{uploader}</p>
       </div>
     </a>
   );
