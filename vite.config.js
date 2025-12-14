a54b5a9215da3c34798e07a407421f9fdf00fbf7
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-13 19:01:51 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // default output folder for Vite
-  },
-  base: "./", // ensures relative paths so GitHub Pages works
+  base: '/MyTube-Frontend/',  // THIS IS THE KEY LINE
 });
