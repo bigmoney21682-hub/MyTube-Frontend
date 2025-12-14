98d4f73284684eb5b1f9d41182b352e44aa24c7f
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-14 17:40:54 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Watch from "./pages/Watch.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Watch from "./pages/Watch";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/watch/:videoId" element={<Watch />} />
-    </Routes>
+    <BrowserRouter basename="/MyTube-Frontend">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/watch/:id" element={<Watch />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
