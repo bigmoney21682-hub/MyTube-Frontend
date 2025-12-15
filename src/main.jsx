e4c43ae69e8d9c5ff4d196d59b2be246006ebcaf
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-14 20:05:21 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-// Import the global CSS here
-import "./index.css";
+import "./index.css";  // <-- add this
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
