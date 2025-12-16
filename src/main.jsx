29d238a39ec02b175638856fea7bfeffb2b0fae0
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-16 08:19:34 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { PlaylistProvider } from "./components/PlaylistContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <PlaylistProvider>
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </PlaylistProvider>
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
