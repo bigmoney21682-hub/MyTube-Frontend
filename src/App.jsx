212484cb9eaf3edc06e4a83eb38f8c74a8b2adc7
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-16 09:40:47 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,3 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Watch from "./pages/Watch";
-// Playlist page can be a stub for now
-import Playlist from "./pages/Playlist";
-
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/watch/:id" element={<Watch />} />
-      <Route path="/playlist" element={<Playlist />} />
-    </Routes>
-  );
+  return <div style={{ color: "white" }}>APP OK</div>;
 }
