ab189abf7b38d0c6d7d430879966d17a70b33eda
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-17 16:29:05 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Watch from "./pages/Watch";
 import Playlist from "./pages/Playlist";
+import Playlists from "./pages/Playlists";  // <-- Add this import
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/watch/:id" element={<Watch />} />
-      <Route path="/playlist" element={<Playlist />} />
+      <Route path="/playlist" element={<Playlist />} /> {/* if you change to parameterized later */}
+      <Route path="/playlist/:id" element={<Playlist />} /> {/* optional better version */}
+      <Route path="/playlists" element={<Playlists />} />  {/* <-- Add this line */}
     </Routes>
   );
 }
