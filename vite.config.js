34ab5c9dc6c8f18df54928de0296b723f73cbfcf
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-16 23:59:42 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MyTube-Frontend/", // 🔴 MUST MATCH REPO NAME EXACTLY
+  base: "/MyTube-Frontend/"
 });
