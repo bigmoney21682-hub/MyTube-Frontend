dbb44e194cb0a1a7e07f5d8c387387c1bb2104bf
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-16 21:11:17 -0500

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,18 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import { usePlaylists } from "./PlaylistContext";
 
 export default function VideoCard({ video, onClick }) {
+  const navigate = useNavigate();
   const { addToPlaylist } = usePlaylists();
 
+  const handleClick = () => {
+    if (onClick) return onClick();
+    navigate(`/watch/${video.id}`);
+  };
+
   return (
-    <div style={{ marginBottom: 12 }}>
+    <div style={{ marginBottom: 16 }}>
       <img
         src={video.thumbnail}
         alt={video.title}
-        onClick={onClick}
-        style={{ width: "100%", cursor: "pointer" }}
+        onClick={handleClick}
+        style={{
+          width: "100%",
+          cursor: "pointer",
+          borderRadius: 8
+        }}
       />
 
-      <h4>{video.title}</h4>
+      <h4 style={{ margin: "8px 0" }}>{video.title}</h4>
 
       <button onClick={() => addToPlaylist(0, video)}>
         ➕ Playlist
