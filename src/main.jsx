d0cc48836400bebffbf99c03a167778d5fe3cb00
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-16 23:17:43 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { PlaylistProvider } from "./components/PlaylistContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PlaylistProvider>
-      <HashRouter>
+    <BrowserRouter basename="/MyTube-Frontend">
+      <PlaylistProvider>
         <App />
-      </HashRouter>
-    </PlaylistProvider>
+      </PlaylistProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
