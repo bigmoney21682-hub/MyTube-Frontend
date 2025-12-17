22676ded312ac4cb0598390844d698f4853579aa
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-17 16:27:54 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/MyTube-Frontend">
+    <BrowserRouter basename="/MyTube-Frontend/">
       <PlaylistProvider>
         <App />
       </PlaylistProvider>
